4637d666c0d163fafcc3676c0ffbf221190422b7
Daniel Silva <dodosilva51@gmail.com>
2021-11-10 08:32:36 -0300

    Up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ExternalExceptionFilter } from '@nestjs/core/exceptions/external-exception-filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new ExternalExceptionFilter());
+
   await app.listen(process.env.PORT || 9000);
 }
 
